b358376d69d977907aee2cf58ef03d4f3fb3bc07
João Gabriel Alves <joao.alves.1129911@sga.pucminas.br>
2022-06-09 17:53:07 -0300

    formulario funcionando

--- src/js/ajuda.js
+++ src/js/ajuda.js
@@ -1,29 +1,23 @@
-//recebendo valores do html
-const form = document.querySelector('.formulario');
-// trigger do submit e declaro as vars q vão receber os valores do form
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  let nome = document.getElementById('nome').value,
-      email = document.getElementById('email').value,
-      msg = document.getElementById('message').value;
+const btn = document.getElementById('enviar');
+const form = document.getElementById('form');
 
-  enviarMsg(nome, email, msg); // chamo a funcção com os dados do form
-  document.querySelector('#formulario').reset(); // limpar os campos
-})
-function enviarMsg(nome, email, msg){
-  Email.send({
-    SecureToken:'88e04528-7b01-4b95-a4a9-1f678182a1e5', // token do smtpjs
-    To:'pmvsihw@gmail.com', // usado pra espelhar o form com os dados inseridos
-    From: 'pmvsihw@gmail.com',
-    Subject: `${nome} acabou de enviar uma mensagem`,
-    Body: `Nome: ${nome}<br>Email: ${email}<br>Mensagem:<br>${msg}`
-  }).then(
-      message => {
-        if (message === 'OK'){
-          alert('Mensagem enviada com sucesso!');
-        } else {
-          alert(message);
-        }
-      }
-  );
-}
+btn.addEventListener('click', function (e){
+    e.preventDefault();
+
+    var itens = {
+        nome: document.getElementById('nome').value,
+        email: document.getElementById('email').value,
+        message: document.getElementById('message').value
+    }
+    const serviceID = "service_gej7skl";
+    const templateID = "template_o7l66aa";
+    const publicKEY = "yv5IegctYrD0NW6Rq";
+
+
+    emailjs.sendForm(serviceID,templateID, form, publicKEY)
+        .then(function(response) {
+            console.log('Enviado!', response.status, response.text);
+        }, function(err) {
+            console.log('Error...', err);
+        });
+});
