42844451ded9dfa7679d1f62bc42855dfafea452
João Gabriel Alves <joao.alves.1129911@sga.pucminas.br>
2022-06-09 12:22:43 -0300

    mudança no código js #51

--- src/js/ajuda.js
+++ src/js/ajuda.js
@@ -1,35 +1,29 @@
+//recebendo valores do html
 const form = document.querySelector('.formulario');
-const input = document.querySelectorAll('.nome, .email, .msg');
-
-function enviarMsg(e){
-
+// trigger do submit e declaro as vars q vão receber os valores do form
+form.addEventListener('submit', e => {
   e.preventDefault();
+  let nome = document.getElementById('nome').value,
+      email = document.getElementById('email').value,
+      msg = document.getElementById('message').value;
 
-  const name = document.querySelector('.nome'),
-        email = document.querySelector('.email'),
-        msg = document.querySelector('.msg');
-
-  // função para enviar Email
+  enviarMsg(nome, email, msg); // chamo a funcção com os dados do form
+  document.querySelector('#formulario').reset(); // limpar os campos
+})
+function enviarMsg(nome, email, msg){
   Email.send({
-    SecureToken:'88e04528-7b01-4b95-a4a9-1f678182a1e5',
-    To:'pmvsihw@gmail.com',
+    SecureToken:'88e04528-7b01-4b95-a4a9-1f678182a1e5', // token do smtpjs
+    To:'pmvsihw@gmail.com', // usado pra espelhar o form com os dados inseridos
     From: 'pmvsihw@gmail.com',
-    Subject: `Resposta do formulário`,
-    Body: `${name.value}<br>${msg.value}<br>Enviado por ${email.value}`
+    Subject: `${nome} acabou de enviar uma mensagem`,
+    Body: `Nome: ${nome}<br>Email: ${email}<br>Mensagem:<br>${msg}`
   }).then(
-    message => {
-      if (message === 'OK'){
-        alert('Enviado com sucesso');
-      } else {
-        alert(message);
+      message => {
+        if (message === 'OK'){
+          alert('Mensagem enviada com sucesso!');
+        } else {
+          alert(message);
+        }
       }
-    }
   );
-
-  input.forEach(input => {
-    input.value = '';
-  });
 }
-
-// trigger pro botão
-form.addEventListener('submit', enviarMsg);
