019ed823fbd77e78a832177fbaa6d29b7aa18dca
João Gabriel Alves <joao.alves.1129911@sga.pucminas.br>
2022-06-10 12:21:48 -0300

    arrumando o codigo js de help.html

--- src/js/ajuda.js
+++ src/js/ajuda.js
@@ -4,15 +4,9 @@ const form = document.getElementById('form');
 btn.addEventListener('click', function (e){
     e.preventDefault();
 
-    var itens = {
-        nome: document.getElementById('nome').value,
-        email: document.getElementById('email').value,
-        message: document.getElementById('message').value
-    }
     const serviceID = "service_gej7skl";
     const templateID = "template_o7l66aa";
     const publicKEY = "yv5IegctYrD0NW6Rq";
-
 
     emailjs.sendForm(serviceID,templateID, form, publicKEY)
         .then(function(response) {
